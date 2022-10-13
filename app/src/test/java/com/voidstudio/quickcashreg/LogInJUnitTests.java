12de088e9e27a93cf5fecb8582955879a2eb1407
Callum MacNeil <cl780851@dal.ca>
2022-10-13 11:31:46 -0300

    Added espresso dependencies in app level gradle, created test classes for logIn activity

---
+++ app/src/test/java/com/voidstudio/quickcashreg/LogInJUnitTests.java
@@ -0,0 +1,13 @@
+package com.voidstudio.quickcashreg;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class LogInJUnitTests {
+}
