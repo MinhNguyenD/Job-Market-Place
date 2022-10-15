f5f7bf5636574865609ebeefac59bd14045e48fe
William Huynh <hy523029@dal.ca>
2022-10-15 18:00:14 -0300

    Testing gradle

--- app/src/main/java/com/voidstudio/quickcashreg/InAppActivity.java
+++ app/src/main/java/com/voidstudio/quickcashreg/InAppActivity.java
@@ -1,7 +1,9 @@
 package com.voidstudio.quickcashreg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,11 @@ public class InAppActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.in_app_activity);
+
+        Intent thisIntent = getIntent();
+        String welcomeMessage = thisIntent.getStringExtra(LogIn.WELCOME);
+        TextView message = findViewById(R.id.textView4);
+        message.setText(welcomeMessage);
     }
 }
--- app/src/main/java/com/voidstudio/quickcashreg/LogIn.java
+++ app/src/main/java/com/voidstudio/quickcashreg/LogIn.java
@@ -1,160 +1,221 @@
 package com.voidstudio.quickcashreg;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
+import android.widget.Button;
+import android.content.SharedPreferences;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LogIn extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String EMPTY_STRING = "";
+    private static final String EMPTY_CREDENTIALS = "Username or password is empty";
     private static final String USER_DOES_NOT_EXIST = "This username does not exist";
     private static final String INCORRECT_PASSWORD = "Incorrect Password!";
+    public static final String WELCOME = "Welcome to In App!";
 
-    private static String alertMessage;
+    private static String alertMessage = "BROKEN";
+    private static String databaseUser;
+
 
     //Success message may be replaced with switch of activities in future iter
     private static final String SUCCESS = "Success";
 
-    private static boolean empty = false;
-    private static boolean userExists = false;
-    private static boolean correctPassword = false;
+    private static boolean empty;
+    private static boolean userExists;
+    private static boolean correctPassword;
 
-    //Firebase Initialization(Commented out till im allowed to use it)
-    //private FirebaseDatabase firebaseDB;
-    //private static final String FIREBASE_URL =
-    // "https://quickcash-bd58f-default-rtdb.firebaseio.com/";
-    //private DatabaseReference firebaseDBReference
+    private static String user;
+    private static String pass;
+
+    private FirebaseDatabase firebaseDB;
+    private static final String FIREBASE_URL =
+            "https://quickcash-bd58f-default-rtdb.firebaseio.com/";
+    private DatabaseReference firebaseDBReference;
+    private DatabaseReference userChild;
+
+//    static SharedPreferences sp;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        // Find the Show Password button and the Password Field
-        Button showPassword = findViewById(R.id.showHidePassword);
-        showPassword.setOnClickListener(LogIn.this);
+        Button backToRegisterScreen = (Button)findViewById(R.id.logInRegisterButton);
+        backToRegisterScreen.setOnClickListener(LogIn.this);
+
+        Button continueButton = (Button)findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(LogIn.this);
+
+        initializeDatabase();
+
+
     }
 
-    public void showHidePassword(Button showPassword, EditText passwordText) {
-        if (passwordText.getText().toString().isEmpty()) {
-            passwordText.setError("Please enter a password!");
-        } else if (showPassword.getText().toString().equals("Show Password")) {
-            showPassword.setText("Hide Password");
-            passwordText.setTransformationMethod(null);
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sp = getSharedPreferences("login", MODE_PRIVATE);
+        if (sp.getBoolean("logged", true)) {
+            gotoInAppActivity();
         } else {
-            showPassword.setText("Show Password");
-            passwordText.setTransformationMethod(new PasswordTransformationMethod());
+            Button login = findViewById(R.id.continueButton);
+            login.setOnClickListener(LogIn.this);
         }
     }
 
-    //protected void initializeDatabase(){
-    //  firebaseDB = FirebaseDatabase.getInstance(FIREBASE_URL)
-    //  firebaseDBReference = firebaseDB.getReferenceFromURL(FIREBASE_URL)
-    //}
+    public void gotoInAppActivity() {
+        Intent inApp = new Intent(this, InAppActivity.class);
+        startActivity(inApp);
+    }
+
+     */
+
+    protected void initializeDatabase(){
+        firebaseDB = FirebaseDatabase.getInstance(FIREBASE_URL);
+        firebaseDBReference = firebaseDB.getReferenceFromUrl(FIREBASE_URL);
+        userChild = firebaseDBReference.child("users");
+    }
 
 
-    protected String getUserName() {
+
+    protected String getUserName(){
         EditText usernameBox = findViewById(R.id.logInUserName);
         return usernameBox.getText().toString().trim();
     }
 
-    protected String getPassword() {
+    protected String getPassword(){
         EditText passwordBox = findViewById(R.id.textPassword);
         return passwordBox.getText().toString().trim();
     }
 
-    protected boolean isEmptyUsername(String username) {
+    protected boolean isEmptyUsername(String username){
         return username.isEmpty();
     }
 
-    protected boolean isEmptyPassword(String password) {
+    protected boolean isEmptyPassword(String password){
         return password.isEmpty();
     }
 
-    protected void emptyCredentials() {
+    protected void emptyCredentials(){
         String username = getUserName();
         String password = getPassword();
-        if (isEmptyPassword(password) || isEmptyUsername(username)) {
+        if(isEmptyPassword(password)||isEmptyUsername(username)){
             empty = true;
-        } else empty = false;
+        }
+        else empty = false;
     }
 
 
     /**
+     *
      * @param username entered username
-     * @return True if username exists in the database
+     * @return userExists if username exists in the database
      */
-    protected static void existingUser(String username) {
+    protected boolean existingUser(String username){
         //UNCOMMENT WHEN CONNECTED TO FIREBASE
-        // firebaseDBReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-        //   @Override
-        //   public void onDataChange(@NonNull DataSnapshot snapshot) {
-        //     if(snapshot.hasChild(userName)){
-        //       usernameExists = true;
-        //     }
-        //     else{
-        //       usernameExists = false;
-        //     }
-        //   }
-        //   @Override
-        //   public void onCancelled(@NonNull DatabaseError error) {
-        //   }
-        // });
+        userChild.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.hasChild(username)){
+                    userExists = true;
+                }
+                else{
+                    userExists = false;
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(LogIn.this,"FAIL",Toast.LENGTH_LONG).show();
+            }
+        });
+        return userExists;
     }
 
-    protected static void passwordMatch(String password) {
+    protected boolean passwordMatch(String password){
         //UNCOMMENT WHEN CONNECTED TO FIREBASE
-        // firebaseDBReference.child("users").child(getUserName()).
-        // addListenerForSingleValueEvent(new ValueEventListener() {
-        //   @Override
-        //   public void onDataChange(@NonNull DataSnapshot snapshot) {
-        //     if(snapshot.hasChild(password)){
-        //       correctPassword = true;
-        //     }
-        //     else{
-        //       correctPassword = false;
-        //     }
-        //   }
-        //   @Override
-        //   public void onCancelled(@NonNull DatabaseError error) {
-        //   }
-        // });
+        firebaseDBReference.child("users").child(getUserName()).
+                addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(password.equals(snapshot.child(password).getKey())){
+                            correctPassword = true;
+                        }
+                        else{
+                            correctPassword = false;
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(LogIn.this,"FAIL",Toast.LENGTH_LONG).show();
+                    }
+                });
+        return correctPassword;
     }
 
-    public static boolean logIn(String username, String password) {
-        existingUser(username);
-        passwordMatch(password);
-        if (empty) {
-            alertMessage = EMPTY_STRING;
+    public static boolean logIn(String username, String password){
+        //Check if user exists
+        //Check if password is correct
+        if(empty){
+            alertMessage = EMPTY_CREDENTIALS;
             return false;
-        } else if (userExists) {
+        }
+        if(!userExists){
             alertMessage = USER_DOES_NOT_EXIST;
             return false;
-        } else if (correctPassword) {
+        }
+        if(!correctPassword){
             alertMessage = INCORRECT_PASSWORD;
             return false;
-        } else {
+        }
+        else{
             alertMessage = SUCCESS;
+            /*
+            // Add username and password to login sharedPreferences state
+            sp.edit().putString("Username", username).apply();
+            sp.edit().putString("Password", password).apply();
+            sp.edit().putBoolean("logged", true).apply();
+             */
             return true;
         }
     }
 
+    protected void switchToRegisterWindow(){
+        Intent registerSwitch = new Intent(LogIn.this, MainActivity.class);
+        startActivity(registerSwitch);
+    }
+
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.showHidePassword) {
-            showHidePassword(findViewById(R.id.showHidePassword), findViewById(R.id.textPassword));
-        }
-
         emptyCredentials();
-        logIn(getUserName(), getPassword());
-        Toast.makeText(LogIn.this, alertMessage, Toast.LENGTH_LONG).show();
-        //Replace toast with new activity in future iterations.
+        if (view.getId() == R.id.logInRegisterButton) {
+            switchToRegisterWindow();
+        } else if(view.getId() == R.id.continueButton) {
+            logIn(getUserName(), getPassword());
+            Toast.makeText(LogIn.this, alertMessage + getPassword() + getUserName(), Toast.LENGTH_LONG).show();
+            //Replace toast with new activity in future iterations
+
+            if (logIn(getUserName(), getPassword())) {
+                Intent inApp = new Intent(LogIn.this, InAppActivity.class);
+                inApp.putExtra(WELCOME, "");
+                startActivity(inApp);
+            }
+        }
     }
 }
