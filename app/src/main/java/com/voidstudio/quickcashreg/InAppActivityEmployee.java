823519f1a77622ac22b1abbba1cfb5d57cc48b63
Lam Tieu <lm436150@dal.ca>
2022-11-02 21:04:01 -0300

    Set up for notification feature

--- app/src/main/java/com/voidstudio/quickcashreg/InAppActivityEmployee.java
+++ app/src/main/java/com/voidstudio/quickcashreg/InAppActivityEmployee.java
@@ -1,16 +1,25 @@
 package com.voidstudio.quickcashreg;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class InAppActivityEmployee extends AppCompatActivity implements View.OnClickListener {
+  String channelID = "Notification";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -36,6 +45,24 @@ public class InAppActivityEmployee extends AppCompatActivity implements View.OnC
     SharedPreferences.Editor editor = sharedPrefs.edit();
     editor.putBoolean(LogInActivity.ISLOGGED, false);
     editor.commit();
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.O)
+  protected void addJobNotification() {
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelID);
+    builder.setContentTitle("New Job Posting Alert!");
+    builder.setContentText("");
+    builder.setTicker("");
+    builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+    // change this to job board activity later
+    Intent jobBoardIntent = new Intent(this, InAppActivityEmployee.class);
+    PendingIntent pIntent = PendingIntent.getActivity(getBaseContext(), 0, jobBoardIntent, PendingIntent.FLAG_IMMUTABLE);
+
+    builder.setContentIntent(pIntent);
+    NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+    manager.createNotificationChannel(new NotificationChannel(channelID, "custom", NotificationManager.IMPORTANCE_HIGH));
+    manager.notify(0, builder.build());
   }
 
   @Override
