1be08ef96521366ad0383584c33cd3e2c344df80
Luke <lk376720@dal.ca>
2022-11-04 17:22:55 -0300

    locationFeatures n such

---
+++ app/src/test/java/com/voidstudio/quickcashreg/LocationUnitTest.java
@@ -0,0 +1,37 @@
+package com.voidstudio.quickcashreg;
+
+import static org.junit.Assert.assertTrue;
+
+import android.location.Location;
+
+import com.voidstudio.quickcashreg.Location.GPS;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class LocationUnitTest {
+    static GPS gps;
+    static Location loc; //= new Location("locationland");
+    @BeforeClass
+    public static void setup() {
+        gps = Mockito.mock(GPS.class);
+        loc = Mockito.mock(Location.class);
+        loc.setLongitude(0.0);
+        loc.setLatitude(0.0);
+    }
+    @Test
+    public void locationIsCorrect(){
+        Location tempLoc = gps.getLocation();
+        assertTrue(loc.getLatitude() == tempLoc.getLatitude() && loc.getLongitude() == tempLoc.getLongitude());
+    }
+}
+
