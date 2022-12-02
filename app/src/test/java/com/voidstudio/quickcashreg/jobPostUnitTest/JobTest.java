492eedcaf33a11a97c01ddaf37349a19bfd18d73
minh <mn549239@dal.ca>
2022-12-01 20:53:31 -0400

    Modify job test to pass

--- app/src/test/java/com/voidstudio/quickcashreg/jobPostUnitTest/JobTest.java
+++ app/src/test/java/com/voidstudio/quickcashreg/jobPostUnitTest/JobTest.java
@@ -16,6 +16,8 @@ public class JobTest {
 
   private static final String JOB_WAGE = "Wage";
 
+  private static final String JOB_DURATION ="1";
+
   private static final String JOB_TAG = "Tag";
 
   private static final String EMPLOYER = "Scumbag";
@@ -38,8 +40,8 @@ public class JobTest {
 
   @Test
   public void jobToString(){
-    Job job = new Job(JOB_NAME,JOB_WAGE,JOB_TAG,EMPLOYER);
-    String expected = JOB_NAME + " " + JOB_WAGE +" " + JOB_TAG + "  Posted by:" + EMPLOYER;
+    Job job = new Job(JOB_NAME,JOB_WAGE,JOB_DURATION, JOB_TAG,EMPLOYER);
+    String expected = JOB_NAME + " Wage:" + JOB_WAGE +" Duration:" + JOB_DURATION + " Tag:" + JOB_TAG + "  Posted by:" + EMPLOYER + " on " + job.getDatePosted();
     assertEquals("To string error", job.toString(), expected);
   }
 
